7cb89010a7ce0f90885f75680a99459895efcbf8
Kashish <kashishjangid71363@gmail.com>
2024-05-27 15:45:14 +0530

    vendor dashboard updates

--- serverfluid3/Routes/DashboardRoutes.js
+++ serverfluid3/Routes/DashboardRoutes.js
@@ -4,6 +4,7 @@ const Auth = require('../Middleware/authUser')
 const dataController = require('../Controller/DashboardController');
 
 dataRoutes.get("/dashboard", Auth, dataController.Dashboard);
+dataRoutes.get("/admindashboard/users", Auth, dataController.Dashboard);
 // dataRoutes.get("/dashboard",  dataController.Dashboard);
 
 module.exports = dataRoutes
