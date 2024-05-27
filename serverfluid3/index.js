e55db4a5aa0848bfa57063f5ffb964503ac94c17
Kashish <kashishjangid71363@gmail.com>
2024-05-27 14:17:37 +0530

    Changes in backend index.js

--- serverfluid3/index.js
+++ serverfluid3/index.js
@@ -19,7 +19,7 @@ app.use(cors({
 
 
 
-mongoose.connect('mongodb+srv://kashishjangid:kashishjangid123@cluster0.mbxux9e.mongodb.net/Fluid2', {
+mongoose.connect('mongodb+srv://kashishjangid:kashishjangid123@cluster0.mbxux9e.mongodb.net/Fluid3', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
